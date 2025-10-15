7f87f983f07367e52922f928938b145e3f357f09
danygabrielmaftei <mafteidany9@gmail.com>
2025-10-15 11:06:31 +0300

    update

---
+++ HabitTrackerApp3-main/src/HabitManagerTest.java
@@ -0,0 +1,66 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabitManagerTest {
+
+    @Test
+    void saveToFile() {
+    }
+
+    @Test
+    void loadFromFile() {
+    }
+
+    @Test
+    void addHabit() {
+    }
+
+    @Test
+    void habitInList() {
+    }
+
+    @Test
+    void removeHabit() {
+    }
+
+    @Test
+    void markHabitAsComplete() {
+    }
+
+    @Test
+    void getDayCounter() {
+    }
+
+    @Test
+    void nextDay() {
+    }
+
+    @Test
+    void getSummary() {
+    }
+
+    @Test
+    void weeklySummary() {
+    }
+
+    @Test
+    void clearStreakList() {
+    }
+
+    @Test
+    void clearTotalDone() {
+    }
+
+    @Test
+    void showAllHabits() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void getHabits() {
+    }
+}
---
+++ HabitTrackerApp3-main/src/HabitTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HabitTest {
+
+    @Test
+    void setDone() {
+    }
+
+    @Test
+    void unDone() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getStreak() {
+    }
+
+    @Test
+    void isDone() {
+    }
+
+    @Test
+    void addStreakToList() {
+    }
+
+    @Test
+    void getStreakList() {
+    }
+
+    @Test
+    void setStreak() {
+    }
+
+    @Test
+    void getTotalDone() {
+    }
+
+    @Test
+    void setTotalDone() {
+    }
+}
